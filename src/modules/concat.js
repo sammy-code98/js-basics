a5f07ad0e803e840885ebbc052e76e61dbd05ba9
sammy-code98 <sammysmart98@yahoo.com>
2022-07-09 22:27:31 +0100

    task done

--- src/modules/concat.js
+++ src/modules/concat.js
@@ -13,7 +13,8 @@ const userAge = 20;
 
 /**
  * Function to Concatenate a string variable with a number variable
- * @type {function}
+ * @function 
+ * @returns {string} - String variable concatenated with a number variable
  */
 
 function concatenate() {
