69ff750cd2fd9864751c2300952e855517036ca2
sammy-code98 <sammysmart98@yahoo.com>
2022-07-21 21:53:00 +0100

    .

--- src/app.js
+++ src/app.js
@@ -2,17 +2,20 @@ const concatenate = require("./modules/concat");
 const isPalindrome = require("./modules/palidrome");
 const sumOfNumbers = require("./modules/arraySum");
 const higherOrderFunctions = require("./modules/hof");
+const sum = require("./modules/complexSum")
 
 // console.log(concatenate());
 // console.log(isPalindrome("45"));
 // console.log(sumOfNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-console.log(
-  "objectWithAgeGreaterThanTwenty ::",
-  higherOrderFunctions.objectWithAgeGreaterThanTwenty
-);
+// console.log(
+//   "objectWithAgeGreaterThanTwenty ::",
+//   higherOrderFunctions.objectWithAgeGreaterThanTwenty
+// );
 
-console.log(
-  "ageLessThanTwentyone::",
-  higherOrderFunctions.ageLessThanTwentyone
-);
-console.log("findAllFemales::", higherOrderFunctions.findAllFemales);
+// console.log(
+//   "ageLessThanTwentyone::",
+//   higherOrderFunctions.ageLessThanTwentyone
+// );
+// console.log("findAllFemales::", higherOrderFunctions.findAllFemales);
+
+console.log(sum(11,4));
--- src/modules/complexSum.js
+++ src/modules/complexSum.js
@@ -1,11 +1,19 @@
+/**
+ * @function - function to add two numbers
+ * @desc Add two numbers, returns 0 if the sum of the two numbers is greater than 21, if a number = 11, convert to 1 and sum up
+ * @param {*} a
+ * @param {*} b
+ * @returns {Number}
+ */
+
 const sum = function complexSum(a, b) {
   if (a + b > 21) {
     return 0;
   } else if (a || b === 11) {
-    return (a || b != 11) + 1 ;
+    return (a || b == 11) + 1;
   } else {
     return a + b;
   }
 };
 
-console.log(sum(2, 11));
+module.exports = sum;
