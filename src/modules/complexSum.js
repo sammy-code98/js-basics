2e4add3ff4f353dab5ca23e27d81a9c3969c541e
sammy-code98 <sammysmart98@yahoo.com>
2022-07-24 19:49:50 +0100

    complex sum

--- src/modules/complexSum.js
+++ src/modules/complexSum.js
@@ -10,7 +10,7 @@ const sum = function complexSum(a, b) {
   if (a + b > 21) {
     return 0;
   } else if (a || b === 11) {
-    return (a || b == 11) + 1;
+    return (a || b === 11)  + 1;
   } else {
     return a + b;
   }
